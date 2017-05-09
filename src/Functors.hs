fc61cc7c1cf05896d0d147692ee58007adeac5b2
Mikhail Tsirikov <t.bachkovsky@gmail.com>
2017-05-09 18:57:29 +0300

    functors, monads

---
+++ src/Functors.hs
@@ -0,0 +1,44 @@
+module Functors where
+
+data Point3D a = Point3D a a a deriving Show
+
+instance Functor Point3D where
+    fmap f (Point3D x y z) = Point3D (f x) (f y) (f z)
+
+data GeomPrimitive a = Point (Point3D a) | LineSegment (Point3D a) (Point3D a) deriving Show
+
+instance Functor GeomPrimitive where
+    fmap f (Point p)           = Point (fmap f p)
+    fmap f (LineSegment p1 p2) = LineSegment (fmap f p1) (fmap f p2)
+
+data Tree' a = Leaf' a | Branch' (Tree' a) a (Tree' a)
+    deriving Show
+
+testTree = Branch' (Leaf' 2) 3 (Leaf' 4)
+
+instance Functor Tree' where
+    fmap g (Leaf' x)       = Leaf' (g x)
+    fmap g (Branch' l x r) = Branch' (fmap g l) (g x) (fmap g r)
+
+data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
+
+instance Functor Tree where
+    fmap f (Leaf m)       = Leaf (f <$> m)
+    fmap f (Branch l m r) = Branch (f <$> l) (f <$> m) (f <$> r)
+
+--instance Functor ((,) s) where
+--    fmap g (x, y) = (x, g y)
+
+data Entry k1 k2 v = Entry (k1, k2) v  deriving Show
+data Map k1 k2 v = Map [Entry k1 k2 v]  deriving Show
+
+instance Functor (Entry k1 k2) where
+    fmap g (Entry keys v) = Entry keys (g v)
+
+instance Functor (Map k1 k2) where
+    fmap g (Map es) = Map $ map (fmap g) es
+
+    {-Законы:
+    fmap id      = id
+    fmap (p . q) = (fmap p) . (fmap q)
+    -}
---
+++ src/Monads.hs
@@ -0,0 +1,67 @@
+module Monads where
+import Control.Monad
+
+data Log a = Log [String] a deriving Show
+
+toLogger :: (a -> b) -> String -> (a -> Log b)
+toLogger f s a = Log [s] (f a)
+
+execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
+execLoggers x f g = Log (m1 ++ m2) x'' where
+    Log m1 x'  = f x
+    Log m2 x'' = g x'
+
+{-
+
+class Monad m where                     -- m - однопараметрический тип (* -> *)
+  return ::   a                 -> m a  -- упаковка значения в контейнер типа m
+
+  (>>=)  :: m a -> (  a -> m b) -> m b  -- монадическое связывание (bind)
+
+  (>>)   :: m a ->  m b         -> m b  -- облегченное связываие (отбрасывает первый аргумент, всегда возвращает второй,
+                                        -- но сохраняет побочные эффекты, вызванные применением связывания к первому аргументу)
+  fail   :: String -> m a               -- прерывание цепочки вычислений
+
+-}
+
+toKleisli :: Monad m => (a -> b) -> (a -> m b)
+toKleisli f x = return (f x)
+--toKleisli f = \x -> return (f x)
+
+returnLog :: a -> Log a
+returnLog a = Log [] a
+
+bindLog :: Log a -> (a -> Log b) -> Log b
+bindLog (Log m1 x) f = Log (m1 ++ m2) x' where
+    (Log m2 x') = f x
+
+instance Functor Log where
+  fmap = liftM
+
+instance Applicative Log where
+  pure = return
+  (<*>) = ap
+
+instance Monad Log where
+    return = returnLog
+    (>>=) = bindLog
+
+execLoggersList :: a -> [a -> Log a] -> Log a
+execLoggersList = foldl (>>=) . return
+
+newtype Identity a = Identity {runIdentity :: a}
+    deriving (Show, Eq)
+
+instance Functor Identity where
+    fmap f x = x >>= return . f
+
+instance Applicative Identity where
+  pure = return
+  Identity f <*> Identity v = Identity (f v)
+
+instance Monad Identity where
+    return x = Identity x
+    Identity x >>= k = k x
+
+wrap'n'succ :: Integer -> Identity Integer
+wrap'n'succ x = Identity (succ x)
