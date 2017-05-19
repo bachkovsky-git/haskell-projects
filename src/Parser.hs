e52a422af39aa3ca2829a01a4f01ff709cf086aa
Mikhail Tsirikov <t.bachkovsky@gmail.com>
2017-05-19 23:19:26 +0300

    parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -29,5 +29,45 @@ ignoreBraces :: Parsec String u a -> Parsec String u b
 ignoreBraces lBr rBr inp = lBr *> inp <* rBr
 
 newtype Parser a = Parser {apply :: String -> [(a, String)]}
+
 parse' :: Parser a -> String -> a
 parse' p = fst . head . apply p
+
+anyChar' :: Parser Char
+anyChar' = Parser f where
+  f ""     = []
+  f (c:cs) = [(c, cs)]
+
+instance Functor Parser where
+  f `fmap` p = Parser fun where
+    fun s = [ (f a, s') | (a, s') <- apply p s]
+
+newtype Prs a = Prs { runPrs :: String -> Maybe (a, String) }
+
+instance Functor Prs where
+  f `fmap` p = Prs fun where
+    fun s = do
+      (a, s') <- runPrs p s
+      return (f a, s')
+
+anyChr :: Prs Char
+anyChr = Prs f where
+  f ""     = Nothing
+  f (c:cs) = Just (c, cs)
+
+
+instance Applicative Parser where
+  pure a = Parser fun where
+    fun s = [(a, s)]
+
+  pf <*> pv = Parser fun where
+    fun s = [(g a, s'') | (g, s') <- apply pf s, (a, s'') <- apply pv s']
+
+instance Applicative Prs where
+  pure  a = Prs f where
+    f s = Just (a, s)
+  pf <*> pv = Prs f where
+    f s = do
+      (g, s') <- runPrs pf s
+      (a, s'') <- runPrs pv s'
+      return (g a, s'')
